cd5125f22ecffa1cd933b877bd7f15396b23c9d5
itprorh66 <itprorh66@gmail.com>
2015-01-15 09:53:40 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Name:        CacheMatrix
+# Purpose:     Computes the inverse of a matrix using caching to improve
+#              performance by implementing the 'Closure' functionality
+#
+# Created:     14/01/2015
+#
+# Licence:     GNU General Public License, version 3 (GPL-3.0)
+#
+#              This program is distributed WITHOUT ANY WARRANTY;
+#              without even the implied warranty of MERCHANTABILITY
+#              or FITNESS FOR A PARTICULAR PURPOSE.
+#-------------------------------------------------------------------------------
 
-## Write a short comment describing this function
+## This function accepts a matrix as input and creates a function to cache
+## the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     m <- NULL
+     set <- function(y) {
+          x <<- y
+          m <<- NULL
+     }
+      get <- function() x
+      setsolve <- function(solve) m <<- solve
+      getsolve <- function() m
+      list(set = set, get = get,
+           setsolve = setsolve,
+           getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## this function utilizes the features of makeCacheMatrix to solve for
+## The inverse of a matrix utilizing a cache to improve performance
+## by not recomputing a matrix element that has already been computed
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+     m <-x$getsolve()
+     if(!is.null(m)){
+          message("getting cached data")
+          return(m)
+     }
+     data <- x$get()
+     m<- solve(data, ...)
+     x$setsolve(m)
+     m
 }
